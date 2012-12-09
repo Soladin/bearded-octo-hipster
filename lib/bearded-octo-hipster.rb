1a5471ccf1bc64200ba013bedbc0ee524a42d5e8
Sol W <capnsol@gmail.com>
2012-12-08 23:04:22 -0800

    editing the testy gem

--- lib/bearded-octo-hipster.rb
+++ lib/bearded-octo-hipster.rb
@@ -1,5 +1,14 @@
-class BearedThing
+class BeardedOctoHipster
   def initialize(banana=:fruit)
     "what is banana? it is #{banana}."
+  end
+
+  def name
+    "BeardedOctoHipster"
+  end
+
+  def a_method
+    some_things = ["bob", "mike", "fart"]
+    some_things.pop
   end
 end
--- spec/bearded-octo-hipster_spec.rb
+++ spec/bearded-octo-hipster_spec.rb
@@ -1,7 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+# describe "BeardedOctoHipster" do
+#   it "fails" do
+#     fail "hey buddy, you should probably rename this file and start specing for real"
+#   end
+# end
+
 describe "BeardedOctoHipster" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "should return it's name" do
+    blonk = BeardedOctoHipster.new
+    blonk.name.should == "BeardedOctoHipster"
   end
 end
